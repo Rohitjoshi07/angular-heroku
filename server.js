3ae72cdba9d945914813e8cdc234c5b43a4a70ae
Rohit joshi <rohitjoshi9july@gmail.com>
2021-11-03 14:22:43 +0530

    server.js updated

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/demo'));
+app.use(express.static(__dirname + '/dist/Demo'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/demo/index.html'));});
+'/dist/Demo/index.html'));});
 app.listen(process.env.PORT || 8080);
